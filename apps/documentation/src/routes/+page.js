8ea6da1536da7e74d92c0bf8063da64f9b1f1a18
Gustavo Zimbrón <pptavozl@gmail.com>
2024-11-04 01:06:50 -0600

    fix redirection?

--- apps/documentation/src/routes/+page.js
+++ apps/documentation/src/routes/+page.js
@@ -1,9 +1,8 @@
-import { dev } from '$app/environment';
 import { redirect } from '@sveltejs/kit';
 
-export const prerender = true;
+//export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
 export function load() {
-	throw redirect(307, `${dev ? '' : '/svelte-actions'}/docs/welcome`);
+	throw redirect(307, `./docs/welcome`);
 }
