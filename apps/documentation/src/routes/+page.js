edc43e5ca628709034fe706157811d8a30a3e744
Gustavo Zimbrón <pptavozl@gmail.com>
2024-11-04 08:47:15 -0600

    redirection by href

--- apps/documentation/src/routes/+page.js
+++ apps/documentation/src/routes/+page.js
@@ -1,9 +1,8 @@
-import { base } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 
 export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
-export function load() {
-	throw redirect(307, `./docs/welcome#${base}`);
+export function load({ url }) {
+	throw redirect(307, `${url.href}/docs/welcome`);
 }
