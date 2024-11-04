26794f157c3b4891e385871efc0e1bcd58aee517
Gustavo Zimbrón <pptavozl@gmail.com>
2024-11-04 01:26:21 -0600

    fix base

--- apps/documentation/src/routes/+page.js
+++ apps/documentation/src/routes/+page.js
@@ -1,11 +1,8 @@
 import { redirect } from '@sveltejs/kit';
-import { PUBLIC_BASE_PATH } from '$env/static/public';
 
 export const prerender = true;
 
-const base = PUBLIC_BASE_PATH || '';
-
 /** @type {import('./$types').PageLoad} */
 export function load() {
-	throw redirect(307, `${base}/docs/welcome`);
+	throw redirect(307, `/docs/welcome`);
 }
