cb8045ea82c5567056229ca1102a713c0add4f36
Gustavo Zimbrón <pptavozl@gmail.com>
2024-11-04 02:03:19 -0600

    sdf

--- apps/documentation/src/routes/+page.js
+++ apps/documentation/src/routes/+page.js
@@ -8,7 +8,7 @@ export function load({ url }) {
 	let redirectionBase = '';
 
 	if (url.origin.includes('github.io')) {
-		redirectionBase = url.origin + '/svelte-actions';
+		redirectionBase = 'https://gzimbron.github.io/svelte-actions';
 	}
 
 	throw redirect(307, `${redirectionBase}/docs/welcome#${base}`);
